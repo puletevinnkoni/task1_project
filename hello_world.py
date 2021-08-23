c6312970e3bfc63747b2d04a423b02b6ed887dd4
Pule Nkoni <puletevinzues@gmail.com>
2021-08-23 23:08:47 +0200

    greeting the world

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,11 @@
-print("Git is Awesome")
+
+
+git = input("Do you think Git is Awesome?")
+
+if git == "yes":
+    print("Use it")
+    print("For professionals")
+else:
+   program = input("Do you recommend another program?")
+if program =="yes":
+    print("what features does it have")
